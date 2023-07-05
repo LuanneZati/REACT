9136a4a133dccdabc4b09098c046055357db76d8
LuanneZati <luanezattilima@hotmail.com>
2023-07-05 14:50:11 -0300

    Desafio01

---
+++ 1_Intro/Desafio01/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+       <h1>Desafio 01</h1>
+       <p>
+        Esse é o novo elemento que eu inseri no meu arquivo App.jsx
+       </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
