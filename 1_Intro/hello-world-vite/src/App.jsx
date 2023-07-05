7a4095c16dcf02bac206e548814df1fa2c9fbd9c
LuanneZati <luanezattilima@hotmail.com>
2023-07-05 14:29:18 -0300

    alteração html

--- 1_Intro/hello-world-vite/src/App.jsx
+++ 1_Intro/hello-world-vite/src/App.jsx
@@ -9,7 +9,17 @@ function App() {
   return (
     <>
       <div>
-        <h1>Hello world 2</h1>
+        <h1>Hello world - Vite</h1>
+        <h2>Tutorial</h2>
+        <h3>
+          - npm create vite@latest <br></br>
+          - Insert name <br/>
+          - React <br></br>
+          - JavaScript <br></br>
+          - Muda para a pasta criada <br />
+          - npm i <br /> 
+          - npm run dev <br />
+        </h3>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
