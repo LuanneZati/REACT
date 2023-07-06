c26af62a4470522b2d63661b817732d9abcef271
LuanneZati <luanezattilima@hotmail.com>
2023-07-06 09:05:30 -0300

    alter

--- 2_Fundamentos/fundamentos/components/TemplateExpressions.jsx
+++ 2_Fundamentos/fundamentos/components/TemplateExpressions.jsx
@@ -1,8 +1,13 @@
 const TemplateExpressions = () => {
     const name = "Luanne"
+    const data = {
+        nome: "Joao",
+        job: "Programmer"
+    }
     return (
         <div>   
-            <h1>Olá {name}, Tudo bem?</h1>
+            <h3>Olá {name}, Tudo bem?</h3>
+            <p>{data.nome} é {data.job}</p>
         </div>
     )
 }
