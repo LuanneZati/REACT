7d0f799a8692276bcdc904c48b34e48a7bdc065b
LuanneZati <luanezattilima@hotmail.com>
2023-07-06 11:08:32 -0300

    Challange 3

---
+++ 3_Avançando/avançando/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import City from './assets/city.jpg'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Avançando em React</h1>
+        {/*Imagem em public */}
+        <div> 
+          <img src="/img1.jpg" alt="Paisagem" />
+        </div>
+        {/*Imagem em assets */}
+        <div> 
+          <img src={City} alt="Cidade " />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
