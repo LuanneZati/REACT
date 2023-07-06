cc9f4fb5723a84b73f894db03b1b4664c9f6ad61
LuanneZati <luanezattilima@hotmail.com>
2023-07-06 14:26:38 -0300

    if ternário

--- 3_Avançando/avançando/src/Components/ConditionalRender.jsx
+++ 3_Avançando/avançando/src/Components/ConditionalRender.jsx
@@ -1,11 +1,21 @@
 import {useState} from "react";
 const ConditionalRender = () => {
     const[x] = useState(true);
-
+    const [name] = useState("João");
   return (
     <div>
         <h1>Isso será exibido?</h1>
         {x && <p>Se x for true sim</p>}
+        <h1>IF ternário</h1>
+        {name == "João" ? (
+          <div>
+            <p>Nome: {name}</p>
+          </div>
+          ) : (
+            <div>
+              <p>O nome não é João</p>
+            </div>
+          )}
     </div>
   );
 };
