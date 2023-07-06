70ce6ef991951a0115151df33e8d722190bc1998
LuanneZati <luanezattilima@hotmail.com>
2023-07-06 08:44:00 -0300

    Primeiro componente exportado e importado

---
+++ 2_Fundamentos/fundamentos/src/App.jsx
@@ -0,0 +1,27 @@
+// components
+import FirstComponent from '../components/FirstComponent'
+
+//styles / CSS
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="card">
+        <h1>Fundamentos React</h1>
+        <FirstComponent />
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
