b0d43c1763fb120465b4d91726484dbd3387e1c8
LuanneZati <luanezattilima@hotmail.com>
2023-07-10 16:14:58 -0300

    random words and categories

--- 7_SecretWord/src/App.jsx
+++ 7_SecretWord/src/App.jsx
@@ -22,7 +22,37 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList)
 
+  const [pickedWord, setPickedWord] = useState("")
+  const [pickedCategory, setPickedCategory] = useState("")
+  const [letters, setLetters] = useState([])
+
+  const pickWordAndCategory = () => {
+    // pick a random category
+    const categories = Object.keys(words)
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)]
+    //pick a random word
+    const word = words[category][Math.floor(Math.random() * words[category].length)]
+  
+    console.log(category, word)
+
+    return {word, category}
+  }
+
   const startGame = () => {
+    // pick word and pick category
+    const { word, category } = pickWordAndCategory();
+
+    //Create an array of letters
+    let wordLetters = word.split("")
+
+    wordLetters = wordLetters.map((l) => l.toUpperCase())
+    console.log(wordLetters)
+
+    //Fill states
+    setPickedWord(word)
+    setPickedCategory(category)
+    setLetters(letters )
+
     setGameStage(stages[1].name)
   } 
 
