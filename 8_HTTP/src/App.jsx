de53846d9e512bfabfeff4b241d5eaa9441dba67
LuanneZati <luanezattilima@hotmail.com>
2023-07-12 13:11:32 -0300

    getting data from API

--- 8_HTTP/src/App.jsx
+++ 8_HTTP/src/App.jsx
@@ -1,10 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
+const url = "http://localhost:3000/products"
+
+console.log(url)
+
 function App() {
+  const [products, setProducts] = useState([])
+  
+  //Resgatando dados
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+      const data = await res.json();
+      setProducts(data);
+    }
+    
+    fetchData();
+  }, []);
+
   return (
     <div className="app">
       <h1>Lista de produtos</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name} - {product.price}</li>
+        ))}
+      </ul>
     </div>
   )
 }
