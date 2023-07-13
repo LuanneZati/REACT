9bb5af70a1f2025e58687000ef38658a35815995
LuanneZati <luanezattilima@hotmail.com>
2023-07-13 15:28:09 -0300

    Loading data

--- 9_Router/src/pages/Product.jsx
+++ 9_Router/src/pages/Product.jsx
@@ -1,13 +1,24 @@
 import { useParams } from "react-router-dom"
+import { useFetch } from "../hooks/useFetch"
 
 const Product = () => {
-  const {id} = useParams()
-  
+    const { id } = useParams()
+    const url = "http://localhost:3000/products/" + id 
+    const { data:product, loading, error } = useFetch(url)
+    console.log(product)
     return (
-    <div>
-        <p>ID do produto: {id}</p>
-    </div>
-  )
+        <div>
+            <p>ID do produto: {id}</p>
+            {error && <p>Ocorreu um erro</p>}
+            {loading && <p>Carregando...</p>}
+            {product && (
+                <div>
+                    <h2>{product.name}</h2>
+                    <p>{product.price}</p>
+                </div>
+            )}
+        </div>
+    )
 }
 
 export default Product
