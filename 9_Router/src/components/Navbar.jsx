29832472ecd37005f3e1322735d74b9010fae755
LuanneZati <luanezattilima@hotmail.com>
2023-07-13 15:49:37 -0300

    Is Active backgroud button

--- 9_Router/src/components/Navbar.jsx
+++ 9_Router/src/components/Navbar.jsx
@@ -1,13 +1,19 @@
 import './Navbar.css'
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 const Navbar = () => {
   return (
     <nav>
         {/* Channge components without redirection */}
-        <Link to='/'>Home </Link>
+        {/* <Link to='/'>Home </Link>
         <Link to='/about'> Sobre</Link>
-        <Link to='/products'> Produto</Link>
+        <Link to='/products'> Produto</Link> */}
+        
+        {/* <NavLink to='/' className={({isActive}) => (isActive ? "" : "")}>Home</NavLink> */}
+        <NavLink to='/'>Home</NavLink>
+        <NavLink to='/about'>Sobre</NavLink>
+        <NavLink to='/products'>Produto</NavLink>
+        
     </nav>
   )
 }
