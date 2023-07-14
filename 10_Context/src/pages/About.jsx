1872995f9c73b01d5bd3247f890066d7983b85a1
LuanneZati <luanezattilima@hotmail.com>
2023-07-14 09:42:16 -0300

    creating provider

--- 10_Context/src/pages/About.jsx
+++ 10_Context/src/pages/About.jsx
@@ -1,9 +1,13 @@
 import './About.css'
+import { useContext } from 'react'
+import { CounterContext } from '../context/CounterContext'
 
 const About = () => {
+  const { counter } = useContext(CounterContext);
   return (
     <div>
       <h1>About</h1>
+      <p>Valor do contador {counter}</p>
     </div>
   )
 }
